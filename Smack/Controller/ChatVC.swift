1cb338ce6dee1828e35c5dd7b395dfd102dd68fb
Rana Ahmed <rana.ahmed@enozom.com>
2017-11-26 10:53:58 +0200

    add swreaveal menu

---
+++ Smack/Controller/ChatVC.swift
@@ -0,0 +1,38 @@
+//
+//  ChatVC.swift
+//  Smack
+//
+//  Created by Hazem Mohamed Magdy on 11/26/17.
+//  Copyright © 2017 Rana. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
